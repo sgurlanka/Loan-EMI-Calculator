0c3ce18849a7e0af7497b9c84dd3a8a0f2be2393
Suraj Gurlanka <surajnsp@gmail.com>
2019-10-20 19:55:32 +0530

    Initial check-in
    Initial check-in

---
+++ src/main/java/com/sg/tools/emi/svc/EmiComputation.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.tools.emi.svc;
+
+/**
+ *
+ * @author suraj
+ */
+public class EmiComputation {
+
+    public static long monthlyEmi(final double principal, final Double rateOfInt, 
+            long time, final boolean isTimeInyears) {
+        if (isTimeInyears) {
+            time = time * 12;
+        }
+
+        final double convertedRate = (rateOfInt / 12) / 100;
+        final double timeFactor = Math.pow((1 + convertedRate), time);
+        final double emi = (principal * convertedRate * timeFactor) / (timeFactor - 1);
+        final long finalEmi = Math.round(emi * 100) / 100;
+
+        return finalEmi;
+    }
+
+    public static long totalInterestPayable(final double principal, final long emi, 
+            long time, final boolean isTimeInyears) {
+        if (isTimeInyears) {
+            time = time * 12;
+        }
+        
+        final double totalInterest = (time * emi) - principal;
+        final long finalTotalInterest = Math.round(totalInterest * 100) / 100;
+        
+        return finalTotalInterest;
+    }
+    
+    public static long totalPayment(final long emi, long time, 
+            final boolean isTimeInyears) {
+        if (isTimeInyears) {
+            time = time * 12;
+        }
+        
+        final double payment = time * emi;
+        final long totalPayment = Math.round(payment * 100) / 100;
+        
+        return totalPayment;        
+    }
+}
